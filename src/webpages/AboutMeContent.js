d6802fbc5f85df7d0389a402a4d1420efe2aa382
Niko Sendek <nikosendek@Nikos-MacBook-Pro.local>
2019-08-17 12:17:13 -0700

    got rid of unneeded styles and changed style names

--- src/webpages/AboutMeContent.js
+++ src/webpages/AboutMeContent.js
@@ -8,25 +8,25 @@ class AboutMeContent extends Component {
         <h2 className="content-header">
           About Me jkn;ljkn;
         </h2>
-        <h2 className="Content-Sub">The FitnessGram™ Pacer Test</h2>
-<p className="Content-Sub">is a multistage aerobic capacity test that progressively gets more difficult as it continues</p>
-<p  className="Content-Sub">The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound.</p>
-<h3 className="Content-Sub">[ding]  </h3>
-        <p className="Content-Sub">
+        <h2 className="content-sub">The FitnessGram™ Pacer Test</h2>
+<p className="content-sub">is a multistage aerobic capacity test that progressively gets more difficult as it continues</p>
+<p  className="content-sub">The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound.</p>
+<h3 className="content-sub">[ding]  </h3>
+        <p className="content-sub">
         Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start
         </p>
-        <h2 className="Content-Sub">The FitnessGram™ Pacer Test</h2>
-<p className="Content-Sub">is a multistage aerobic capacity test that progressively gets more difficult as it continues</p>
-<p  className="Content-Sub">The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound.</p>
-<h3 className="Content-Sub">[ding]  </h3>
-        <p className="Content-Sub">
+        <h2 className="content-sub">The FitnessGram™ Pacer Test</h2>
+<p className="content-sub">is a multistage aerobic capacity test that progressively gets more difficult as it continues</p>
+<p  className="content-sub">The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound.</p>
+<h3 className="content-sub">[ding]  </h3>
+        <p className="content-sub">
         Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start
         </p>
-        <h2 className="Content-Sub">The FitnessGram™ Pacer Test</h2>
-<p className="Content-Sub">is a multistage aerobic capacity test that progressively gets more difficult as it continues</p>
-<p  className="Content-Sub">The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound.</p>
-<h3 className="Content-Sub">[ding]  </h3>
-        <p className="Content-Sub">
+        <h2 className="content-sub">The FitnessGram™ Pacer Test</h2>
+<p className="content-sub">is a multistage aerobic capacity test that progressively gets more difficult as it continues</p>
+<p  className="content-sub">The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound.</p>
+<h3 className="content-sub">[ding]  </h3>
+        <p className="content-sub">
         Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start
         </p>
       </div>

63801ef2a8c89d8d3cc7692e7672db5742de824d
nsendek <nsendek@Nikos-MBP.hsd1.co.comcast.net>
2020-12-15 09:21:54 -0700

    Updates

---
+++ precache-manifest.ed6715f87f8b4d60e30765e3c934cf5a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc1f72a382cde7426c70d50e3c6a320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b674799712a93b9b5496",
+    "url": "/static/css/main.cf021eb3.chunk.css"
+  },
+  {
+    "revision": "1cdffaf74db10d6fc1e7",
+    "url": "/static/js/2.92671ceb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.92671ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b674799712a93b9b5496",
+    "url": "/static/js/main.81ee6d5a.chunk.js"
+  },
+  {
+    "revision": "7864f64be44bde160f0b",
+    "url": "/static/js/runtime-main.c731a418.js"
+  },
+  {
+    "revision": "7964b467493c4d2c98ea0adf571d8ed6",
+    "url": "/static/media/facebook.7964b467.png"
+  },
+  {
+    "revision": "9d65f3dedb064f54b2b1ad254eac1741",
+    "url": "/static/media/github.9d65f3de.png"
+  },
+  {
+    "revision": "9a71fff536ffe0e67a8e236e0bc64386",
+    "url": "/static/media/instagram.9a71fff5.png"
+  },
+  {
+    "revision": "a24a83484ba4fdeeeabc37d25b551ffb",
+    "url": "/static/media/linkedin.a24a8348.png"
+  },
+  {
+    "revision": "172a93b17c6d8d93186a3fb120d668ca",
+    "url": "/static/media/pic1.172a93b1.png"
+  },
+  {
+    "revision": "9e9cb6ff916356d97975eafe897bfd8c",
+    "url": "/static/media/pic2.9e9cb6ff.png"
+  },
+  {
+    "revision": "6680798765360774a697d94da1e2924e",
+    "url": "/static/media/pic4.66807987.png"
+  },
+  {
+    "revision": "6c89a04aaa7aff9545bb820b44650701",
+    "url": "/static/media/pic5.6c89a04a.png"
+  },
+  {
+    "revision": "6b4a4e6125a4804cc1521a622fea36b3",
+    "url": "/static/media/portfolio.6b4a4e61.pdf"
+  },
+  {
+    "revision": "cebeb4042d31417440c3f53096696027",
+    "url": "/static/media/resume.cebeb404.pdf"
+  },
+  {
+    "revision": "72ae70e8ea36a74e6dd4772eb45fab0c",
+    "url": "/static/media/test2.72ae70e8.png"
+  }
+]);

50871aabc3e606432c4226a399f6c9f262bc7194
nsendek <nsendek1999@gmail.com>
2021-11-03 14:46:41 -0600

    static/images generate thumbnails

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const CopyWebPackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const sharp = require('sharp');
 
 const webpack = require('webpack');
 require('dotenv').config();
@@ -19,14 +19,14 @@ return {
     },
     module: {
       rules: [
-        {
-          test: /\.(jpg|wav|gif|png|svg)$/,
-          exclude: /node_modules/,
-          loader: 'file-loader',
-          options: {
-            outputPath: 'static/assets/'
-          }
-        },
+        // {
+        //   test: /\.(jpg|wav|gif|png|svg)$/,
+        //   exclude: /node_modules/,
+        //   loader: 'file-loader',
+        //   options: {
+        //     outputPath: 'static/assets/'
+        //   }
+        // },
         // {
         //   test: /\.svg$/,
         //   exclude: /node_modules/,
@@ -107,7 +107,13 @@ return {
           },
           { // files in static should be transfered 1:1 directly to new static in dist
             from: 'static',
-            to: "static"
+            to: "static",
+          },
+          //  https://stackoverflow.com/questions/54217627/using-webpack-to-optimise-unreferenced-images-img-loader
+          { // files in static/images need a created thumbnail version of the original (i.e. 300px wide)
+            from: 'static/images/*.(png|jpg)',
+            to: '[path][name]_thumb[ext]',
+            transform: content => sharp(content).resize(300).toBuffer(),
           },
         ]
       }),

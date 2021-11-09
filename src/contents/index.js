639f3e63f9309d1de62aecc660b09fa7257fc52b
nsendek <nsendek1999@gmail.com>
2021-11-09 15:51:22 -0700

    merge simple and component variables into one

--- src/contents/index.js
+++ src/contents/index.js
@@ -3,45 +3,33 @@ import P2 from "./project2";
 import P3 from "./project3";
 import P4 from "./project4";
 
-const simple = [
+export default [
   {
-    id: "project1",
     title : "Project 1",
     path : "/projects/project1",
     thumbnail: 'https://i.imgur.com/OdiNNyym.jpg',
-    summary : ""
+    summary : "",
+    component :  P1
   },
   {
-    id: "project2",
     title : "Project 2",
     path : "/projects/project2",
     thumbnail: 'https://i.imgur.com/67r0FdBm.jpg',
-    summary : ""
+    summary : "",
+    component :  P2
   },
   {
-    id: "project3",
     title : "Project 3",
     path : "/projects/project3",
     thumbnail: 'https://i.imgur.com/SVDaEjIm.jpg',
-    summary : ""
+    summary : "",
+    component :  P3
   },
   {
-    id: "project4",
     title : "Project 4",
     path : "/projects/project4",
     thumbnail: 'https://i.imgur.com/bahJfL9m.jpg',
-    summary : ""
+    summary : "",
+    component :  P4
   },
 ]
-
-const components =  {
-  project1 : P1,
-  project2 : P2,
-  project3 : P3,
-  project4 : P4
-}
-
-export {
-  simple,
-  components
-}

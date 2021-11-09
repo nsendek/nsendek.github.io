0787b1266a005b47bf1cc6ef89f1ce796f3bbb0a
nsendek <nsendek1999@gmail.com>
2021-11-09 13:26:33 -0700

    updates to routing behaviors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 /* eslint-disable indent */
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import {HashRouter} from 'react-router-dom';
+import {HashRouter, useLocation} from 'react-router-dom';
 
 import ParticleWall from "./components/particle-wall";
 import ContactIcons from "./components/contact-icons";
 import Menu from "./components/menu";
 import Views from "./views";
 
+const ScrollRestore = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const Website = () => (
     <React.Fragment>
 
       {/* Using Hash routes because github.io doesn't allow routing configuration */}
       <HashRouter hashType="noslash">
+        <ScrollRestore />
         <ParticleWall />
         <Views />
+
+        <Menu />
       </HashRouter>
 
-      <Menu />
+      
       <ContactIcons />
       
     </React.Fragment>
